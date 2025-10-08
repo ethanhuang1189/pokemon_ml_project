f59b338f697d0c1ad7d668e8a6a58d63043b04be
ekh35 <ekh35@drexel.edu>
2025-10-08 15:28:45 -0400

    Fixed play_one to run properly

--- play_one.py
+++ play_one.py
@@ -1,34 +1,43 @@
 import asyncio
-from poke_env import LocalhostServerConfiguration, Player, ShowdownServerConfiguration, AccountConfiguration
-from poke_env.player import RandomPlayer
+from poke_env import RandomPlayer
+from poke_env.data import GenData
 
-#LOCAL_SERVER = ShowdownServerConfiguration(server_url="http://localhost:8000")
+
 
 
 async def main():
-    random_player = RandomPlayer()
+    Bot_Naila = RandomPlayer()
     second_player = RandomPlayer()
-    p1 = RandomPlayer(
-        start_timer_on_battle_start=True,
-        account_configuration=AccountConfiguration("Bot_Naila", "Naila"),
-        server_configuration=ShowdownServerConfiguration,
-    )
+    #p1 = RandomPlayer(
+    #    start_timer_on_battle_start=True,
+    #    account_configuration=AccountConfiguration("Bot_Naila", "Naila"),
+    #    server_configuration=ShowdownServerConfiguration,
+    #)
 
-    p2 = RandomPlayer(
-        start_timer_on_battle_start=True,
-        account_configuration=AccountConfiguration("peeepoo_man", "Raeh147611"),
-        server_configuration=ShowdownServerConfiguration,
-    )
-    await random_player.battle_against(second_player, n_battles=1)
+    #p2 = RandomPlayer(
+    #    start_timer_on_battle_start=True,
+    #    account_configuration=AccountConfiguration("peeepoo_man", "Raeh147611"),
+    #    server_configuration=ShowdownServerConfiguration,
+    #)
+    print("Starting Battle\n")
+    await Bot_Naila.battle_against(second_player, n_battles=100)
     #await p1.battle_against(p2, n_challenges=1)
     #await p1.complete_current_battle()
     #for battle_tag, battle in p1.battles.items():
-    for battle_tag, battle in random_player.battles.items():
-        print(f"Format: {battle_tag}")
+    print(
+    f"Player {Bot_Naila.username} won {Bot_Naila.n_won_battles} out of {Bot_Naila.n_finished_battles} played"
+    )
+    print(
+        f"Player {second_player.username} won {second_player.n_won_battles} out of {second_player.n_finished_battles} played"
+    )
+
+    # Looping over battles
+
+    for battle_tag, battle in Bot_Naila.battles.items():
         if (battle.won == True):
-            print(f"Winner is: {random_player}")
+            print(f"Bot_Naila won")
         else:
-            print(f"Winner is: {second_player}")
+            print("second_player won")
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(main())
